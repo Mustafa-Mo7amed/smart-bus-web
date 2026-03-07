eb4972ffbd087bf54759d8cedd06ec351a7105c7
mostafa <mostaamohamedd985@gmail.com>
2026-03-07 13:21:24 +0200

    chore(routing-config): add routing config for login page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component, signal } from '@angular/core';
-import { LoginComponent } from "./features/auth/login/login.component";
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [LoginComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class App {
-  
+
 }
---
+++ src/app/app.routes.ts
@@ -0,0 +1,19 @@
+import { Routes } from "@angular/router";
+import { LoginComponent } from "./features/auth/login/login.component";
+
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'auth/login',
+        pathMatch: 'full'
+    },
+    {
+        path: 'auth',
+        children: [
+            {
+                path: 'login',
+                component: LoginComponent
+            }
+        ]
+    }
+]
