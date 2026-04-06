f3885bc33830b90862ebbafdbd20d73efa0d446c
mostafa <mostaamohamedd985@gmail.com>
2026-04-07 01:04:36 +0200

    feat(layout): restructure app layout with header and navigation bar components
    - Create NavBarComponent with initial navigation items and styles
    - Update index.html to include Google Fonts and Material Icons
    - Enhance global styles in styles.scss for better typography and layout
    - Create HeaderComponent with basic structure and styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./shared/layout/header/header.component";
+import { NavBarComponent } from "./shared/layout/nav-bar/nav-bar.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, NavBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
